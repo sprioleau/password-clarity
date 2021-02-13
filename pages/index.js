919923ec06f0888debc1200cdfffd9d189927937
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-02-13 06:51:55 -0500

    Add background image and blur filter

--- pages/index.js
+++ pages/index.js
@@ -40,9 +40,6 @@ const Home = () => {
 		setSuggestions(suggestions);
 	};
 
-	console.log("passwordScore:", passwordScore);
-	console.log("typeof passwordScore:", typeof passwordScore);
-
 	const togglePasswordVisibility = () => {
 		setShowPassword(!showPassword);
 	};
@@ -56,6 +53,19 @@ const Home = () => {
 		};
 
 		return colorMap[passwordScore];
+	};
+
+	const getBlurAmount = () => {
+		if (!passwordScore || !password) return "5";
+
+		const blurMap = {
+			1: "5",
+			2: "4",
+			3: "3",
+			4: "0",
+		};
+
+		return blurMap[passwordScore];
 	};
 
 	return (
@@ -72,14 +82,49 @@ const Home = () => {
 				<meta name="theme-color" content="#ffffff" />
 			</Head>
 
-			<Flex direction="column" justify="flex-start" height="100vh" width="100%" my={12}>
-				<Flex justify="center">
+			<Flex direction="column" justify="center" height="100vh" width="100%" position="relative" overflow="hidden">
+				{/* <Flex justify="center" mb={12}>
 					<IconButton size="lg" colorScheme="blue" variant="solid" type="submit" onClick={toggleColorMode}>
 						{colorMode === "light" ? <RiMoonLine /> : <RiSunLine />}
 					</IconButton>
-				</Flex>
-				<Container as="section" width="100%" maxW={600} px={8} py={12} boxShadow="xl" p="6" rounded="lg">
-					<Flex as="main">
+				</Flex> */}
+				<Container
+					as="section"
+					width="100%"
+					maxW={600}
+					px={8}
+					py={12}
+					my={12}
+					boxShadow="xl"
+					p="6"
+					rounded="lg"
+					/* backdropFilter="blur(5px)" */
+					backgroundColor="whiteAlpha.600"
+				>
+					<Flex
+						className="background-image"
+						backgroundImage="url(https://picsum.photos/id/1018/1920/1080.webp)"
+						/* filter={`blur(${getBlurAmount()})`} */
+						position="absolute"
+						top="50%"
+						left="50%"
+						height="100%"
+						width="100%"
+						transform="translate(-50%, -50%)"
+						zIndex="-2"
+					/>
+					<Flex
+						className="blur-filter"
+						style={{ backdropFilter: `blur(${getBlurAmount()}px)` }}
+						position="absolute"
+						top="50%"
+						left="50%"
+						height="105%"
+						width="105%"
+						transform="translate(-50%, -50%)"
+						zIndex="-1"
+					/>
+					<Flex as="main" zIndex="1">
 						<form onSubmit={handleSubmit} style={{ width: "100%" }}>
 							<Stack spacing={4}>
 								<Heading
@@ -120,7 +165,9 @@ const Home = () => {
 								</FormControl>
 								<Progress value={passwordScore * 25} size="xs" colorScheme={getColorScheme()} />
 
-								{suggestions && password.length > 8 && suggestions.map((suggestion) => <Text>{suggestion}</Text>)}
+								{suggestions &&
+									password.length > 8 &&
+									suggestions.map((suggestion) => <Text key={suggestion}>{suggestion}</Text>)}
 
 								<Button size="lg" colorScheme="blue" variant="solid" type="submit">
 									Get score
