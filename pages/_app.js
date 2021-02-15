37254c69bc7eb912642fda23e463807acdde5df7
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-02-15 06:40:49 -0500

    Update styling, use better images and use next Image component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
-
 import theme from "../theme/theme";
+import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }) {
 	return (
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import {
 	Input,
 	Stack,
@@ -10,25 +11,18 @@ import {
 	Text,
 	Heading,
 	Tooltip,
-	IconButton,
 	InputGroup,
 	Progress,
-	useColorMode,
+	InputRightElement,
 } from "@chakra-ui/react";
 import zxcvbn from "zxcvbn";
-import { BiHide, BiShow } from "react-icons/bi";
-import { RiSunLine, RiMoonLine } from "react-icons/ri";
 
 const Home = () => {
 	const [password, setPassword] = useState("");
 	const [showPassword, setShowPassword] = useState("");
-	const [passwordScore, setPasswordScore] = useState("");
+	const [passwordScore, setPasswordScore] = useState(0);
 	const [suggestions, setSuggestions] = useState("");
-	const { colorMode, toggleColorMode } = useColorMode();
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
+	const [photoId, setPhotoId] = useState(2);
 
 	const handleChangePassword = (e) => {
 		setPassword(e.target.value);
@@ -56,17 +50,23 @@ const Home = () => {
 	};
 
 	const getBlurAmount = () => {
-		if (!passwordScore || !password) return "5";
+		const defaultBlur = "10";
+		if (!passwordScore || !password) return defaultBlur;
 
 		const blurMap = {
-			1: "5",
-			2: "4",
-			3: "3",
+			1: defaultBlur,
+			2: "6",
+			3: "2",
 			4: "0",
 		};
 
 		return blurMap[passwordScore];
 	};
+
+	useEffect(() => {
+		const randomId = (length) => Math.ceil(Math.random() * length);
+		setPhotoId(randomId(5));
+	}, [setPhotoId]);
 
 	return (
 		<Flex as="div" className="app">
@@ -82,100 +82,101 @@ const Home = () => {
 				<meta name="theme-color" content="#ffffff" />
 			</Head>
 
-			<Flex direction="column" justify="center" height="100vh" width="100%" position="relative" overflow="hidden">
-				{/* <Flex justify="center" mb={12}>
-					<IconButton size="lg" colorScheme="blue" variant="solid" type="submit" onClick={toggleColorMode}>
-						{colorMode === "light" ? <RiMoonLine /> : <RiSunLine />}
-					</IconButton>
-				</Flex> */}
+			<Flex
+				direction="column"
+				justify="center"
+				height="100vh"
+				width="100%"
+				position="relative"
+				overflow="hidden"
+				z-index="5"
+			>
 				<Container
 					as="section"
 					width="100%"
-					maxW={600}
+					maxW={500}
 					px={8}
 					py={12}
 					my={12}
 					boxShadow="xl"
 					p="6"
 					rounded="lg"
-					/* backdropFilter="blur(5px)" */
-					backgroundColor="whiteAlpha.600"
+					backgroundColor="whiteAlpha.800"
 				>
-					<Flex
-						className="background-image"
-						backgroundImage="url(https://picsum.photos/id/1018/1920/1080.webp)"
-						/* filter={`blur(${getBlurAmount()})`} */
-						position="absolute"
-						top="50%"
-						left="50%"
-						height="100%"
-						width="100%"
-						transform="translate(-50%, -50%)"
-						zIndex="-2"
-					/>
-					<Flex
-						className="blur-filter"
-						style={{ backdropFilter: `blur(${getBlurAmount()}px)` }}
-						position="absolute"
-						top="50%"
-						left="50%"
-						height="105%"
-						width="105%"
-						transform="translate(-50%, -50%)"
-						zIndex="-1"
-					/>
 					<Flex as="main" zIndex="1">
-						<form onSubmit={handleSubmit} style={{ width: "100%" }}>
+						<form style={{ width: "100%" }}>
 							<Stack spacing={4}>
-								<Heading
-									as="h1"
-									size="3xl"
-									textAlign="center"
-									bgGradient="linear(to-l, #7928CA, #FF0080)"
-									bgClip="text"
-									fontWeight="extrabold"
-									lineHeight="1.2"
-								>
+								<Heading as="h1" size="lg" textAlign="center" fontWeight="extrabold" lineHeight="0.8">
 									Password Clarity
 								</Heading>
+								<Text as="p" textAlign="center" mb={2}>
+									Change the password to clarify the image.
+								</Text>
 
 								<FormControl id="password">
-									<InputGroup size="lg">
+									<InputGroup size="md">
 										<Input
+											autoFocus={true}
+											pr="4rem"
+											type={showPassword ? "text" : "password"}
+											placeholder="Enter password"
 											value={password}
 											onChange={handleChangePassword}
-											type={showPassword ? "text" : "password"}
-											autoFocus
+											focusBorderColor="#179848"
 										/>
-										<Tooltip
-											label={`${showPassword ? "Hide" : "Show"} password`}
-											fontSize="md"
-											placement="bottom"
-											hasArrow
-										>
-											<IconButton
-												colorScheme="blue"
-												aria-label="Show/Hide Password"
-												size="lg"
-												icon={showPassword ? <BiHide /> : <BiShow />}
+										<InputRightElement width="4rem">
+											<Button
+												h="1.75rem"
+												width="3rem"
+												mr={0}
+												size="sm"
+												colorScheme="whatsapp"
 												onClick={togglePasswordVisibility}
-											/>
-										</Tooltip>
+												p={2}
+											>
+												<Tooltip
+													label={`${showPassword ? "Hide" : "Show"} password`}
+													fontSize="md"
+													placement="bottom"
+													hasArrow
+												>
+													{showPassword ? "Hide" : "Show"}
+												</Tooltip>
+											</Button>
+										</InputRightElement>
 									</InputGroup>
 								</FormControl>
-								<Progress value={passwordScore * 25} size="xs" colorScheme={getColorScheme()} />
+								<Progress value={password ? passwordScore * 25 : 0} size="xs" colorScheme={getColorScheme()} />
 
 								{suggestions &&
 									password.length > 8 &&
 									suggestions.map((suggestion) => <Text key={suggestion}>{suggestion}</Text>)}
-
-								<Button size="lg" colorScheme="blue" variant="solid" type="submit">
-									Get score
-								</Button>
 							</Stack>
 						</form>
 					</Flex>
 				</Container>
+			</Flex>
+			<Flex zIndex="-1">
+				<Flex
+					className="blur-filter"
+					style={{ backdropFilter: `blur(${getBlurAmount()}px)` }}
+					position="absolute"
+					top="50%"
+					left="50%"
+					height="105%"
+					width="105%"
+					transform="translate(-50%, -50%)"
+					zIndex="-1"
+				/>
+				<Image
+					className="background-image"
+					src={`/images/${photoId}.jpg`}
+					alt="background image"
+					layout="fill"
+					quality="80"
+					objectPosition="center"
+					priority
+				/>
 			</Flex>
 		</Flex>
 	);
